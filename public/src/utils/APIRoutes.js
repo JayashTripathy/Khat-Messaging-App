7fe31148cff0e7d9245a8414eee3af45cec1fcf0
Jayash Tripathy <jayashtripathy371@gmail.com>
2022-08-01 00:30:53 +0530

    Fixed

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,7 +1,7 @@
-export const host = "https://khat-app.herokuapp.com"
-export const registerRoute = `${host}/api/auth/register`
-export const loginRoute = `${host}/api/auth/login`
-export const setAvatarRoute = `${host}/api/auth/setAvatar`
-export const allUserRoutes = `${host}/api/auth/allUsers`
-export const addMessage = `${host}/api/message/addMsg`
-export const getAllMessageRoute = `${host}/api/message/getAllMessage`
+
+export const registerRoute = `/api/auth/register`
+export const loginRoute = `/api/auth/login`
+export const setAvatarRoute = `/api/auth/setAvatar`
+export const allUserRoutes = `/api/auth/allUsers`
+export const addMessage = `/api/message/addMsg`
+export const getAllMessageRoute = `/api/message/getAllMessage`
