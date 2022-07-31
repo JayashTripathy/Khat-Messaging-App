8e983a31d1bc34755d6c7d27cac4de9a8b685527
Jayash Tripathy <jayashtripathy371@gmail.com>
2022-08-01 00:14:24 +0530

    fixed

--- index.js
+++ index.js
@@ -1,65 +1,59 @@
- require('dotenv').config();
- const path = require("path")
+require("dotenv").config();
+const path = require("path");
 const express = require("express");
- const cors = require('cors');
- const mongoose = require('mongoose');
- const app = express();
- const userRoutes = require("./routes/userRoutes")
- const messageRoute = require("./routes/messageRoutes")
- const socket = require("socket.io")
- 
- app.use(cors({origin: "*"}))
- app.use(express.static('public/build'))
- 
- 
- app.use(express.json());
- 
- app.use("/api/auth", userRoutes)
- app.use("/api/message", messageRoute)
- 
-mongoose.connect(process.env.MONGO_URL, {
+const cors = require("cors");
+const mongoose = require("mongoose");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messageRoutes");
+const socket = require("socket.io");
+
+app.use(cors({ origin: "*" }));
+app.use(express.static("public/build"));
+
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/message", messageRoute);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=> {
-    console.log("DB connected")
-}).catch((err) => {
-    console.log(err.message)
-})
+  })
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-const PORT = process.env.PORT 
-const server = app.listen(PORT , ()=> {
-    console.log(`Server Started on PORT..${PORT}`)
-})
+const PORT = process.env.PORT;
+const server = app
+  .listen(PORT, () => {
+    console.log(`Server Started on PORT..${PORT}`);
+  })
+   
 
 const io = socket(server, {
-    cors: {
-        origin: "*",
-         credentials: true,
-         
-         
-}
-    
-})
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
 
 global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
-    global.chatSocket = socket;
-    socket.on("add-user", (userId) => {
-        onlineUsers.set(userId, socket.id)
-        
-    })
-    
-    socket.on("send-msg", (data) => {
-        const sendUserSocket = onlineUsers.get(data.to)
-        if(sendUserSocket) {
-            socket.to(sendUserSocket).emit("msg-receive", data.msg)
-        }
-    })
-})
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
 
-
-
-
-
-
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-receive", data.msg);
+    }
+  });
+});
